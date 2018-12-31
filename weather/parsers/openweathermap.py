d034f6aebea746effe638da87d9b78034b4a2a8f
Dmitry Kashlev <mkashlev@gmail.com>
2018-12-31 11:14:10 -0800

    ...

--- weather/parsers/openweathermap.py
+++ weather/parsers/openweathermap.py
@@ -28,31 +28,31 @@ def get_weather_for_resort(resort_id):
     if len(weather_data) > 0:
         if len(weather_data['weather']) > 0:
             weather = weather_data['weather'][0]
-            if weather['id']: data_map['label_id'] = weather['id']
-            if weather['main']: data_map['label'] = weather['main']
-            if weather['description']: data_map['description'] = weather['description']
+            if 'id' in weather: data_map['label_id'] = weather['id']
+            if 'main' in weather: data_map['label'] = weather['main']
+            if 'description' in weather: data_map['description'] = weather['description']
         if 'main' in weather_data:
             main = weather_data['main']
-            if main['temp']: data_map['temperature'] = main['temp']
-            if main['pressure']: data_map['pressure'] = main['pressure']
-            if main['humidity']: data_map['humidity'] = main['humidity']
-            if main['temp_min']: data_map['temperature_min'] = main['temp_min']
-            if main['temp_max']: data_map['temperature_max'] = main['temp_max']
+            if 'temp' in main: data_map['temperature'] = main['temp']
+            if 'pressure' in main: data_map['pressure'] = main['pressure']
+            if 'humidity' in main: data_map['humidity'] = main['humidity']
+            if 'temp_min' in main: data_map['temperature_min'] = main['temp_min']
+            if 'temp_max' in main: data_map['temperature_max'] = main['temp_max']
         if 'visibility' in weather_data: data_map['visibility'] = weather_data['visibility']
         if 'wind' in weather_data:
             wind = weather_data['wind']
-            if wind['speed']: data_map['wind_speed'] = wind['speed']
-            if wind['deg']: data_map['wind_dir'] = wind['deg']
+            if 'speed' in wind: data_map['wind_speed'] = wind['speed']
+            if 'deg' in wind: data_map['wind_dir'] = wind['deg']
         if 'clouds' in weather_data and 'all' in weather_data['clouds']:
             data_map['cloudiness'] = weather_data['clouds']['all']
         if 'rain' in weather_data:
             rain = weather_data['rain']
-            if rain['1h']: data_map['rain_last_1h'] = rain['1h']
-            if rain['3h']: data_map['rain_last_3h'] = rain['3h']
+            if '1h' in rain: data_map['rain_last_1h'] = rain['1h']
+            if '3h' in rain: data_map['rain_last_3h'] = rain['3h']
         if 'snow' in weather_data:
-            rain = weather_data['rain']
-            if snow['1h']: data_map['snow_last_1h'] = snow['1h']
-            if snow['3h']: data_map['snow_last_3h'] = snow['3h']
+            snow = weather_data['snow']
+            if '1h' in snow: data_map['snow_last_1h'] = snow['1h']
+            if '3h' in snow: data_map['snow_last_3h'] = snow['3h']
         if 'dt' in weather_data: data_map['data_calculated_at'] = datetime.utcfromtimestamp(weather_data['dt']).strftime('%Y-%m-%d %H:%M:%S')
     cols = ','.join(data_map.keys())
     vals = data_map.values()
